b2509678102c6772a5b6eb27b84fcf4dfa2a2194
Uranium2 <antoine.tavernier@epita.fr>
2014-06-21 14:16:59 +0200

    recup du fucking truc

---
+++ Kcaspher/Kcaspher/AI/AI_W1L1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Projet_2._0.AI
+{
+    class AI_W1L1
+    {
+        public List<AI_basic> AI_w1l1 = new List<AI_basic>();
+        public List<Rectangle> ListHitbox;
+
+        public AI_W1L1()
+        {
+            AI_w1l1.Add(new AI_basic(Content_Manager.getInstance().Textures["enemy1"], Content_Manager.getInstance().Textures["enemy2"], new Rectangle(Res.gI().ScaleX(222), Res.gI().ScaleY(500), Res.gI().ScaleX(40), Res.gI().ScaleY(40)), new Vector2(3, 0), 300));
+            AI_w1l1.Add(new AI_basic(Content_Manager.getInstance().Textures["enemy1"], Content_Manager.getInstance().Textures["enemy2"], new Rectangle(Res.gI().ScaleX(500), Res.gI().ScaleY(700), Res.gI().ScaleX(40), Res.gI().ScaleY(40)), new Vector2(3, 0), 300));
+        }
+
+        public void update(GameTime gametime)
+        {
+            foreach (AI_basic AI in AI_w1l1)
+            {
+                AI.update(gametime);
+            }
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            foreach (AI_basic AI in AI_w1l1)
+            {
+                AI.Draw(spritebatch);
+            }
+        }
+        public List<Rectangle> getListRectangle() 
+        {
+            ListHitbox = new List<Rectangle>();
+            foreach (AI_basic AI in AI_w1l1)
+	        {
+                ListHitbox.Add(AI.Hitbox);
+	        }
+            return ListHitbox;
+        }
+    }
+}
---
+++ Kcaspher/Kcaspher/AI/AI_moderate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Projet_2._0
+{
+    class AI_moderate : Movable
+    {
+        public Texture2D texture;
+        public Rectangle hitbox;
+        public Rectangle aggro;
+        bool casperIsIntersecting = false;
+
+
+        public AI_moderate(Texture2D texture, Rectangle hitbox)
+            : base(texture, hitbox)
+        {
+            this.texture = texture;
+            this.hitbox = hitbox;
+            aggro = new Rectangle(hitbox.X - Res.gI().ScaleX(350) / 2, hitbox.Y - Res.gI().ScaleY(350)/2, Res.gI().ScaleX(500), Res.gI().ScaleY(500));
+            Direction = new Vector2(0, 0);
+            Velocity = new Vector2(2.5f,2.5f);
+            Position = new Vector2(0, 0);
+        }
+
+        public void update(GameTime gametime, Casper casper)
+        {
+            if (aggro.Intersects(casper.Hitbox))
+            {
+                if (!casperIsIntersecting)
+                {
+                    SoundManager.alert.Play();
+                    casperIsIntersecting = true;
+                }
+                if (!hitbox.Intersects(casper.Hitbox))
+                {
+                    Direction = new Vector2(casper.Position.X - hitbox.X, casper.Position.Y - hitbox.Y);
+                    Direction.Normalize();
+                    Position.X = hitbox.X;
+                    Position.Y = hitbox.Y;
+                    Position += Direction * Velocity;
+                    hitbox.X = (int)Position.X;
+                    hitbox.Y = (int)Position.Y;
+
+                }
+            }
+            aggro.X = hitbox.X - Res.gI().ScaleX(500) / 2;
+            aggro.Y = hitbox.Y - Res.gI().ScaleY(500) / 2;
+            if (!aggro.Intersects(casper.Hitbox))
+            {
+                casperIsIntersecting = false;
+            }
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            //spritebatch.Draw(texture, aggro, Color.Black);
+            spritebatch.Draw(texture, hitbox, Color.White);
+            
+        }
+    }
+}
